3895862c47caceb42820a4ba09609ab8e912592f
Sami mhd. <221086@students.au.edu.pk>
2024-10-09 23:15:16 +0500

    Weather forecast API

---
+++ api.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+import requests
+from flask_cors import CORS  # Import CORS
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+API_KEY = '5ccc694960fa44a14945d4e559ed8b98'
+BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+
+@app.route('/api/weather', methods=['GET'])
+def get_weather():
+    city = request.args.get('city')
+    if not city:
+        return jsonify({"error": "City is required"}), 400
+
+    url = f"{BASE_URL}?q={city}&appid={API_KEY}&units=metric"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        weather_info = {
+            "city": data['name'],
+            "temperature": data['main']['temp'],
+            "description": data['weather'][0]['description'],
+            "humidity": data['main']['humidity'],
+            "wind_speed": data['wind']['speed'],
+        }
+        return jsonify(weather_info), 200
+    else:
+        return jsonify({"error": "City not found"}), 404
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
