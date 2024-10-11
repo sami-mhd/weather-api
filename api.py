4027cc009d4d72c51d3a591823ecc9a798f7e432
Sami mhd. <221086@students.au.edu.pk>
2024-10-11 23:38:04 +0500

    Updates

--- api.py
+++ api.py
@@ -32,3 +32,46 @@ def get_weather():
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
+
+
+
+# from flask import Flask, jsonify, request
+# import requests
+# from flask_cors import CORS
+# from dotenv import load_dotenv
+# import os
+
+# # Load environment variables from .env file
+# load_dotenv()
+
+# app = Flask(__name__)
+# CORS(app)
+
+# # Retrieve the API key from the environment variable
+# API_KEY = os.getenv('API_KEY')  
+# BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+
+# @app.route('/api/weather', methods=['GET'])
+# def get_weather():
+#     city = request.args.get('city')
+#     if not city:
+#         return jsonify({"error": "City is required"}), 400
+
+#     url = f"{BASE_URL}?q={city}&appid={API_KEY}&units=metric"
+#     response = requests.get(url)
+
+#     if response.status_code == 200:
+#         data = response.json()
+#         weather_info = {
+#             "city": data['name'],
+#             "temperature": data['main']['temp'],
+#             "description": data['weather'][0]['description'],
+#             "humidity": data['main']['humidity'],
+#             "wind_speed": data['wind']['speed'],
+#         }
+#         return jsonify(weather_info), 200
+#     else:
+#         return jsonify({"error": "City not found"}), 404
+
+# if __name__ == '__main__':
+#     app.run(port=5000, debug=True)
